3ab690527fdaf748b0bd4265455fe21df98afa4e
David Reke <david.e.reke@gmail.com>
2020-09-09 09:53:53 -0400

    fix edit exercise

--- client/src/components/edit-exercise.component.js
+++ client/src/components/edit-exercise.component.js
@@ -23,7 +23,7 @@ export default class EditExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get(BACKEND_URL + "/exercises/" + this.props.match.params.id)
+    axios.get(BACKEND_URL + "exercises/" + this.props.match.params.id)
     .then((response) => {
           this.setState({
             username: response.data.username,
